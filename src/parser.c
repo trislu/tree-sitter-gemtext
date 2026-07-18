ebd2c2eff318575d5d0cd091a23588e1e670d31e
room2 <lukai@mail.ustc.edu.cn>
2026-07-18 11:55:05 +0800

    bump version to 0.1.1

--- src/parser.c
+++ src/parser.c
@@ -513,7 +513,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_gemtext(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
